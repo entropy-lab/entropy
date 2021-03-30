d4a25ac053ea59e7c7fc0c04d68b4db21365395d
Guy Kerem <guy@quantum-machines.co>
2021-03-30 18:51:53 +0300

    remove dashboard

--- quaentropy/results/dashboard/plot.py
+++
@@ -1,8 +0,0 @@
-from typing import Union
-
-from api.model import PlotGenerator
-
-
-class PanelPlotGenerator(PlotGenerator):
-    def __init__(self, data, type_or_action: Union[str]) -> None:
-        super().__init__()

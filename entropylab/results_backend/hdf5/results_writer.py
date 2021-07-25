974ac14ccdbf184dbec2f911b7e983de64158e0a
urig <uri.goldstein@gmail.com>
2021-07-25 12:44:36 +0300

    A unit test for writing results data to HDF5

--- entropylab/results_backend/hdf5/results_writer.py
+++ entropylab/results_backend/hdf5/results_writer.py
@@ -5,9 +5,6 @@ from entropylab import RawResultData
 
 class ResultsWriter:
 
-    def __file_session(self):
-        return h5py.File(self.path, 'a', self.driver, **self.kwds)
-
     def __init__(self):
         pass
 
@@ -18,6 +15,7 @@ class ResultsWriter:
             dset = file.create_dataset(
                 name=dset_name,
                 data=result.data)
-            # shape=(1,),
-            # dtype=type(result.data))
+            # TODO: does `label` really belong in an attribute?
             dset.attrs.create('label', result.label)
+            # TODO: what do we do with `story`?
+            # TODO: strings come back a byte arrays...?
--- entropylab/results_backend/hdf5/tests/test_results_writer.py
+++ entropylab/results_backend/hdf5/tests/test_results_writer.py
@@ -1,16 +1,38 @@
+import os
+from random import randrange
+from typing import Any
+
+import h5py
 import numpy as np
+import pytest
 
 from entropylab import RawResultData
 from entropylab.results_backend.hdf5.results_writer import ResultsWriter
 
 
-def test_write_result_int_data():
-    # arrange
-    target = ResultsWriter()
-    result = RawResultData(label="foo", data=np.arange(1))
-    result.stage = 0
-    result.story = "A long time ago in a galaxy far, far away..."
-    # act
-    target.write_result(0, result)
-    # assert
-    # clean up
+@pytest.mark.parametrize(
+    "data", [
+        42, True, 3.14159265359, -160000000000000, np.int64(42)
+    ])
+def test_write_result_with_scalar_data(data: Any):
+    filename = ""
+    try:
+        # arrange
+        target = ResultsWriter()
+        experiment_id = randrange(10000000)
+        result = RawResultData(label="foo", data=data)
+        result.stage = randrange(1000)
+        result.story = "A long time ago in a galaxy far, far away..."
+
+        # act
+        target.write_result(experiment_id, result)
+
+        # assert
+        filename = f"experiment_{experiment_id}.hdf5"
+        dset_name = f"stage_{result.stage}"
+        with h5py.File(filename, 'r') as file:
+            dset = file.get(dset_name)
+            assert dset[()] == data
+    finally:
+        # clean up
+        os.remove(filename)

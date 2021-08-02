e5c5903d46228637da193e613b5600ba3b2835b0
urig <uri.goldstein@gmail.com>
2021-08-02 13:33:11 +0300

    Initial migration now skips existing tables. All tests are green.

--- entropylab/results_backend/sqlalchemy/tests/test_migrations.py
+++ entropylab/results_backend/sqlalchemy/tests/test_migrations.py
@@ -10,24 +10,30 @@ from entropylab import SqlAlchemyDB
 
 @pytest.mark.parametrize(
     "db", [
-        "./empty.db",
-        "./initial.db",
-        "./with_saved_in_hdf5_col.db"])
+        None,  # new db
+        "empty.db",
+        "initial.db",  # revision 1318a586f31d
+        "with_saved_in_hdf5_col.db"  # revision 04ae19b32c08
+    ])
 def test_migrations(db: str):
     # arrange
-    db_under_test = get_test_file_name(db)
-    copyfile(db, db_under_test)
+    if db is not None:
+        db_under_test = get_test_file_name(db)
+        copyfile(os.path.join("./db_templates/", db), db_under_test)
+    else:
+        db_under_test = get_test_file_name("new.db")
     try:
         # act
         SqlAlchemyDB(path=db_under_test, echo=True)
         # assert
         conn = sqlite3.connect(db_under_test)
         cur = conn.execute("SELECT sql FROM sqlite_master WHERE name = 'Results'")
-        sql = cur.fetchone()
-        assert "saved_in_hdf5" in sql
+        res = cur.fetchone()
+        conn.close()
+        assert "saved_in_hdf5" in res[0]
     finally:
+        # clean up
         os.remove(db_under_test)
-        pass
 
 
 def get_test_file_name(filename):

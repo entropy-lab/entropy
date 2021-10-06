2257498b8febcd72a4d08553a54d8bb480118d3a
Uri Goldstein <uri.goldstein@gmail.com>
2021-10-06 16:50:26 +0300

    New test fixtures in conftest.py replace helper funcs in test_utils.py
    Refactored test_db_upgrader with new fixtures for project dirs
    
    Refactored test_db.py with new fixtures for project dirs
    
    Refactored test_migrations.py with new fixtures for project dirs
    
    Integrated new test fixtures into high level tests
    
    test_utils.py replaced by new test fixtures in conftest.py

--- entropylab/results_backend/sqlalchemy/tests/test_db.py
+++ entropylab/results_backend/sqlalchemy/tests/test_db.py
@@ -1,22 +1,13 @@
 import pytest
 
 from entropylab import SqlAlchemyDB, RawResultData
-from entropylab.results_backend.sqlalchemy.tests.test_utils import (
-    delete_if_exists,
-    create_test_project,
-)
 
 
-def test_save_result_raises_when_same_result_saved_twice(request):
+def test_save_result_raises_when_same_result_saved_twice(initialized_project_dir_path):
     # arrange
-    test_project_dir = create_test_project(request)
-    try:
-        db = SqlAlchemyDB(test_project_dir, echo=True, enable_hdf5_storage=True)
-        raw_result = RawResultData(stage=1, label="foo", data=42)
+    db = SqlAlchemyDB(initialized_project_dir_path, enable_hdf5_storage=True)
+    raw_result = RawResultData(stage=1, label="foo", data=42)
+    db.save_result(0, raw_result)
+    with pytest.raises(ValueError):
+        # act & assert
         db.save_result(0, raw_result)
-        with pytest.raises(ValueError):
-            # act & assert
-            db.save_result(0, raw_result)
-    finally:
-        # clean up
-        delete_if_exists(test_project_dir)
--- entropylab/results_backend/sqlalchemy/tests/test_migrations.py
+++ entropylab/results_backend/sqlalchemy/tests/test_migrations.py
@@ -1,52 +1,41 @@
 import pytest
 
 from entropylab import SqlAlchemyDB
-from entropylab.results_backend.sqlalchemy.tests.test_utils import (
-    create_test_project,
-    delete_if_exists,
-)
 
 
 @pytest.mark.parametrize("path", [None, ":memory:"])
 def test_ctor_creates_up_to_date_schema_when_in_memory(path: str):
-    try:
-        # act
-        target = SqlAlchemyDB(path=path, echo=True)
-        # assert
-        cur = target._engine.execute(
-            "SELECT sql FROM sqlite_master WHERE name = 'Results'"
-        )
-        res = cur.fetchone()
-        cur.close()
-        assert "saved_in_hdf5" in res[0]
-    finally:
-        delete_if_exists("./entropy.hdf5")
+    # act
+    target = SqlAlchemyDB(path=path, echo=True)
+    # assert
+    cur = target._engine.execute("SELECT sql FROM sqlite_master WHERE name = 'Results'")
+    res = cur.fetchone()
+    cur.close()
+    assert "saved_in_hdf5" in res[0]
 
 
 @pytest.mark.parametrize(
-    "db_template, expected_to_raise",
+    "initialized_project_dir_path",
     [
-        (None, False),  # new db
-        ("./db_templates/empty.db", False),  # existing but empty
-        ("./db_templates/initial.db", True),  # revision 1318a586f31d
-        ("./db_templates/with_saved_in_hdf5_col.db", False),  # revision 04ae19b32c08
+        None,  # new db
+        "./db_templates/empty.db",  # existing but empty
+        "./db_templates/with_saved_in_hdf5_col.db",  # latest revision: 04ae19b32c08
     ],
+    indirect=True,
 )
 def test_ctor_ensures_latest_migration(
-    db_template: str, expected_to_raise: bool, request
+    initialized_project_dir_path,
 ):
-    # arrange
-    if db_template is not None:
-        test_project_dir = create_test_project(request, db_template)
-    else:
-        test_project_dir = create_test_project(request)
-    try:
-        if expected_to_raise:
-            with pytest.raises(RuntimeError):
-                # act & assert
-                SqlAlchemyDB(path=test_project_dir, echo=True)
-        else:
-            SqlAlchemyDB(path=test_project_dir, echo=True)
-    finally:
-        # clean up
-        delete_if_exists(test_project_dir)
+    SqlAlchemyDB(initialized_project_dir_path)
+
+
+@pytest.mark.parametrize(
+    "initialized_project_dir_path",
+    ["./db_templates/initial.db"],  # revision 1318a586f31d
+    indirect=True,
+)
+def test_ctor_throws_when_db_is_not_up_to_date(
+    initialized_project_dir_path,
+):
+    with pytest.raises(RuntimeError):
+        SqlAlchemyDB(initialized_project_dir_path)

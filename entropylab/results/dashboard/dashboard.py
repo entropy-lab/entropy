27c0131b57fd32e42819fba15b5b7490888bbeaa
Uri Goldstein <uri.goldstein@gmail.com>
2021-11-01 17:32:57 +0200

    Round robin color for plot.ly plots
    Fixed bug where plot.generator is None

--- entropylab/results/dashboard/dashboard.py
+++ entropylab/results/dashboard/dashboard.py
@@ -12,22 +12,24 @@ from entropylab.results_backend.sqlalchemy.project import project_name, project_
 MAX_EXPERIMENTS_NUM = 10000
 EXPERIMENTS_PAGE_SIZE = 6
 
-app = dash.Dash(__name__)
 
+def init(app, path):
+    """Initialize the results dashboard Dash app to display an Entropy project
 
-def init(path):
+    :param app the Dash app to initialize with the dashboard contents
+    :param path path where the Entropy project to be used resides."""
+
     # App setup
 
     _title = f"Entropy - {project_name(path)} [{project_path(path)}]"
     app.title = _title
 
+    # Fetching data from DB
+
     _dashboard_data_reader = SqlalchemyDashboardDataReader(SqlAlchemyDB(path))
     _experiments = pd.DataFrame(
         _dashboard_data_reader.get_last_experiments(MAX_EXPERIMENTS_NUM)
     )
-
-    # Fetching data from DB
-
     # TODO: How to filter this column when its values are emoji?
     _experiments["success"] = _experiments["success"].apply(
         lambda x: "✔️" if x else "❌"
@@ -35,6 +37,7 @@ def init(path):
     _records = _experiments.to_dict("records")
 
     # Components
+
     _table = table(app, _records)
 
     _tabs = dcc.Tabs(
@@ -48,67 +51,48 @@ def init(path):
     @app.callback(
         Output("plot-tabs", "children"),
         Input("experiments-table", "selected_row_ids"),
-        # State("table", "data"),
     )
-    def get_selected_row_ids(selected_row_ids):
-        colors = ["red", "green", "blue", "hotpink", "yellow", "maroon", "lilach"]
+    def render_plot_tabs_from_selected_experiments_table_rows(selected_row_ids):
+        colors = ["red", "green", "blue", "hotpink", "purple", "maroon", "brown"]
         result = []
         if selected_row_ids:
             for exp_id in selected_row_ids:
                 plots = _dashboard_data_reader.get_plot_data(exp_id)
-                # if plots:
                 for plot in plots:
-                    # if plot.generator:
-                    figure = go.Figure()
-                    plot.generator.plot_plotly(
-                        figure,
-                        plot.plot_data,
-                        color="blue",
-                        # label=f"{plot.experiment_id}",
-                    )
-                    result.append(
-                        dcc.Tab(
-                            label=f"Tab {plot.id}",
-                            children=[
-                                html.H3(f"Plot {plot.id}"),
-                                dcc.Graph(id=f"graph-{plot.id}-tabs", figure=figure),
-                            ],
+                    if plot.generator:
+                        figure = go.Figure()
+                        plot.generator.plot_plotly(
+                            figure,
+                            plot.plot_data,
+                            color=colors[len(result) % len(colors)],
                         )
-                    )
-
-                    # result.append(
-                    #     dcc.Tab(
-                    #         label=f"Tab {plot.id}",
-                    #         children=[
-                    #             html.H3(f"Plot {plot.id}"),
-                    #             dcc.Graph(
-                    #                 id=f"graph-{plot.id}-tabs",
-                    #                 figure={
-                    #                     "data": [
-                    #                         {
-                    #                             "x": [1, 2, 3],
-                    #                             "y": [3, 1, 2],
-                    #                             "type": "bar",
-                    #                             "marker": {
-                    #                                 "color": colors[plot.id]
-                    #                             },
-                    #                         }
-                    #                     ]
-                    #                 },
-                    #             ),
-                    #         ],
-                    #     )
-                    # )
+                        result.append(
+                            dcc.Tab(
+                                label=f"Tab {plot.id}",
+                                children=[
+                                    html.H3(f"Plot {plot.id}"),
+                                    dcc.Graph(
+                                        id=f"graph-{plot.id}-tabs", figure=figure
+                                    ),
+                                ],
+                            )
+                        )
         return result
 
     # App layout
 
     app.layout = html.Div(
-        children=[html.H1(_title), _table, html.Div(id="output_div"), _tabs],
+        children=[
+            html.H1(_title),
+            _table,
+            html.Div(children=[_tabs, html.Div(id="overlay")]),
+        ],
         className="main",
     )
 
 
 if __name__ == "__main__":
-    init("tests")
-    app.run_server(debug=True)
+    """ This is the dash app that hosts our results dashboard """
+    _app = dash.Dash(__name__)
+    init(_app, "tests")
+    _app.run_server(debug=True)

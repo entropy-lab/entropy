2cfd4d67425d9e5401e050c58f98e39fffb7040a
Uri Goldstein <uri.goldstein@gmail.com>
2021-12-05 19:30:23 +0200

    Persisting aggregate plot (separated clicks from list of plot keys)

--- entropylab/results/dashboard/dashboard.py
+++ entropylab/results/dashboard/dashboard.py
@@ -125,68 +125,51 @@ def build_dashboard_app(path):
         )
 
     @_app.callback(
-        Output("aggregate-tab", "children"),
-        Output("remove-buttons", "children"),
         Output("plot-keys-to-combine", "data"),
-        Input({"type": "remove-button", "index": ALL}, "n_clicks"),
         Input("add-button", "n_clicks"),
+        Input({"type": "remove-button", "index": ALL}, "n_clicks"),
         State("plot-tabs", "active_tab"),
-        State("plot-figures", "data"),
         State("plot-keys-to-combine", "data"),
     )
-    def add_or_remove_plot_in_combined_plot(
-        n_clicks1, n_clicks2, active_tab, plot_figures, plot_keys_to_combine
+    def add_or_remove_plot_keys_based_on_click_events(
+        n_clicks1, n_clicks2, active_tab, plot_keys_to_combine
     ):
         plot_keys_to_combine = plot_keys_to_combine or []
         prop_id = dash.callback_context.triggered[0]["prop_id"]
-        # trigger was a click on the "Add >>" button
         if prop_id == "add-button.n_clicks" and active_tab:
             active_plot_key = active_tab.replace("plot-tab-", "")
             if active_plot_key not in plot_keys_to_combine:
                 plot_keys_to_combine.append(active_plot_key)
-            tabs, remove_buttons = build_aggregate_graph_and_remove_buttons(
-                plot_figures, plot_keys_to_combine
-            )
-            return (
-                tabs,
-                remove_buttons,
-                plot_keys_to_combine,
-            )
-        # trigger was a click on one of the remove buttons
         elif "remove-button" in prop_id:
             id_dict = json.loads(prop_id.replace(".n_clicks", ""))
             remove_plot_key = id_dict["index"]
             if remove_plot_key in plot_keys_to_combine:
                 plot_keys_to_combine.remove(remove_plot_key)
+        return plot_keys_to_combine
 
-                tabs, remove_buttons = build_aggregate_graph_and_remove_buttons(
-                    plot_figures, plot_keys_to_combine
-                )
-                return (
-                    tabs,
-                    remove_buttons,
-                    plot_keys_to_combine,
-                )
-
-        # default case
+    @_app.callback(
+        Output("aggregate-tab", "children"),
+        Output("remove-buttons", "children"),
+        Input("plot-keys-to-combine", "data"),
+        State("plot-figures", "data"),
+    )
+    def build_combined_plot_from_plot_keys(plot_keys_to_combine, plot_figures):
+        if plot_keys_to_combine and len(plot_keys_to_combine) > 0:
+            combined_figure = make_subplots(specs=[[{"secondary_y": True}]])
+            remove_buttons = []
+            for plot_id in plot_keys_to_combine:
+                figure = plot_figures[plot_id]["figure"]
+                color = plot_figures[plot_id]["color"]
+                combined_figure.add_trace(figure["data"][0])
+                button = build_remove_button(plot_id, color)
+                remove_buttons.append(button)
+            combined_figure.update_layout(dark_plot_layout)
+            return dcc.Graph(figure=combined_figure, responsive=True), remove_buttons
         else:
             return (
                 [build_aggregate_tab_placeholder()],
                 [html.Div()],
-                plot_keys_to_combine,
             )
-
-    def build_aggregate_graph_and_remove_buttons(plot_figures, plot_keys_to_combine):
-        combined_figure = make_subplots(specs=[[{"secondary_y": True}]])
-        remove_buttons = []
-        for plot_id in plot_keys_to_combine:
-            figure = plot_figures[plot_id]["figure"]
-            color = plot_figures[plot_id]["color"]
-            combined_figure.add_trace(figure["data"][0])
-            button = build_remove_button(plot_id, color)
-            remove_buttons.append(button)
-        combined_figure.update_layout(dark_plot_layout)
-        return dcc.Graph(figure=combined_figure, responsive=True), remove_buttons
 
     def build_remove_button(plot_id, color):
         return dbc.Button(

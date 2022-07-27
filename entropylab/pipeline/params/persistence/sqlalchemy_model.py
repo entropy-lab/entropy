c633de95d1d1ff206e402f0be40f6050b75d7059
Uri Goldstein <uri.goldstein@gmail.com>
2022-07-27 17:47:09 +0300

    Bringing in SqlAlchemy model for paramstore persistence

---
+++ entropylab/pipeline/params/persistence/sqlalchemy_model.py
@@ -0,0 +1,60 @@
+import uuid
+
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import JSONB, UUID
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.sql import expression
+
+Base = declarative_base()
+
+""" Auto-generate UTC timestamp in PostgreSQL """
+
+
+class UtcNow(expression.FunctionElement):
+    """SqlAlchemy function to generate UTC timestamp on the server-side (Postgres)
+    Source: https://docs.sqlalchemy.org/en/14/core/compiler.html#utc-timestamp-function
+    """
+
+    type = DateTime()
+    inherit_cache = True
+
+
+@compiles(UtcNow, "postgresql")
+def pg_utcnow(element, compiler, **kw):
+    return "TIMEZONE('utc', CURRENT_TIMESTAMP)"
+
+
+""" Compiling to sqlite for testing purposes """
+
+
+@compiles(UtcNow, "sqlite")
+def sqlite_utcnow(element, compiler, **kw):
+    return "CURRENT_TIMESTAMP"
+
+
+@compiles(UUID, "sqlite")
+def sqlite_uuid(element, compiler, **kw):
+    return "TEXT"
+
+
+@compiles(JSONB, "sqlite")
+def sqlite_jsonb(element, compiler, **kw):
+    return "JSON"
+
+
+""" SqlAlchemy ORM model for ParamStore """
+
+
+class Commit(Base):
+    __tablename__ = "commit"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    timestamp = Column(
+        DateTime(timezone=False), nullable=False, server_default=UtcNow()
+    )
+    label = Column(String(256))
+    params = Column(JSONB, nullable=False)
+    tags = Column(JSONB, nullable=False)
+
+    def __repr__(self):
+        return f"<Commit(commit_id={self.id}, label={self.label})>"
--- entropylab/pipeline/params/persistence/sqlalchemy_persistence.py
+++ entropylab/pipeline/params/persistence/sqlalchemy_persistence.py
@@ -1,15 +1,33 @@
+from typing import Optional
+
+import jsonpickle
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from entropylab.pipeline.params.persistence.persistence import Persistence
+from entropylab.pipeline.params.persistence.sqlalchemy_model import Commit
 
 
 class SqlAlchemyPersistence(Persistence):
+    def __init__(self, url: Optional[str] = None):
+        self.__session_maker = sessionmaker(
+            bind=create_engine(
+                url,
+                json_serializer=jsonpickle.encode,
+                json_deserializer=jsonpickle.decode,
+            )
+        )
+
     def close(self):
-        pass
+        self.__session_maker.close_all()
 
     def get_commit(self, commit_id, commit_num):
         pass
 
     def get_latest_commit(self):
-        pass
+        with self.__session_maker() as session:
+            commit = session.query(Commit).order_by(Commit.timestamp.desc()).first()
+            return commit
 
     def commit(self, commit, label, dirty_keys):
         pass
